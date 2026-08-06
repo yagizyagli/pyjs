6794eb87d7fa6704ef45c995b8c7c01b32aafdb3
Yağız Yağlı <yagizyagli3447@gmail.com>
2026-08-06 23:28:19 +0300

    Update __init__.py

--- python/pyjs/__init__.py
+++ python/pyjs/__init__.py
@@ -4,7 +4,7 @@ PyJS
 Python <-> JavaScript bridge framework.
 """
 
-from .bridge import (
+from .core.bridge import (
     Bridge,
     Client,
     Server,
@@ -14,66 +14,43 @@ from .bridge import (
     MiddlewarePipeline,
 )
 
-from .protocol import (
+from .core.protocol import (
     Packet,
     Request,
     Response,
     Event,
-
     PacketType,
     PacketPriority,
     PacketStatus,
-
     Protocol,
-
     Codec,
     JSONCodec,
     CodecRegistry,
-
     PROTOCOL_NAME,
     PROTOCOL_VERSION,
 )
 
-
 __version__ = "1.0.0"
 
-
 __all__ = [
-
-    # Core
     "Bridge",
     "Client",
     "Server",
     "Router",
     "Dispatcher",
-
-
-    # Middleware
     "Middleware",
     "MiddlewarePipeline",
-
-
-    # Protocol models
     "Packet",
     "Request",
     "Response",
     "Event",
-
-
-    # Protocol
     "Protocol",
     "PacketType",
     "PacketPriority",
     "PacketStatus",
-
-
-    # Codec
     "Codec",
     "JSONCodec",
     "CodecRegistry",
-
-
-    # Version
     "PROTOCOL_NAME",
     "PROTOCOL_VERSION",
 ]
