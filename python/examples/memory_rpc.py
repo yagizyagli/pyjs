e512c02b51d74e0e08404808804d51703deede80
Yağız Yağlı <yagizyagli3447@gmail.com>
2026-08-06 23:04:18 +0300

    Update memory_rpc.py

--- python/examples/memory_rpc.py
+++ python/examples/memory_rpc.py
@@ -1,5 +1,11 @@
-bridge_b.expose("add", add)
+import asyncio
 
-result = await bridge_a.call("add", 5, 10)
 
-print(result)
+async def main():    
+    result = await bridge_a.call("add", 5, 10)
+
+    print(result)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
