28ea77813edb3f7fec49eb7c52685e4c8ef9d37a
Yağız Yağlı <yagizyagli3447@gmail.com>
2026-08-06 18:27:50 +0300

    Create version.py

---
+++ python/pyjs/core/protocol/version.py
@@ -0,0 +1,15 @@
+"""
+PyJS Protocol Version
+"""
+
+from __future__ import annotations
+
+PROTOCOL_NAME = "pyjs"
+
+PROTOCOL_VERSION = "1.0"
+
+PROTOCOL_MAGIC = "PYJS"
+
+SUPPORTED_VERSIONS = (
+    "1.0",
+)
